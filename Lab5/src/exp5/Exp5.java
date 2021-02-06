4df7a8f16e89e27e6e3b69a63381d2f14faba4db
sitara8900 <mullapudisitara8@gmail.com>
2021-02-06 20:09:39 +0530

    Experiment 5

---
+++ Lab5/src/exp5/Exp5.java
@@ -0,0 +1,52 @@
+package exp5;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class Exp5 {
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/employee","root","");
+
+			String query="update emp set sal=? where empno=?";
+
+			PreparedStatement upsmt=con.prepareStatement(query);
+
+			upsmt.setString(1, "200000");
+			upsmt.setString(2, "104");
+
+			int rows=upsmt.executeUpdate();
+			System.out.println(rows+" row(s) are updated.");
+
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery("select * from emp");
+			System.out.println("empno\tename\t\tsal\t\tdept");
+
+			while (rs.next()) {
+	            int empno = rs.getInt("empno");
+	            String name = rs.getString("name");
+	            String sal = rs.getString("sal");
+	            String dept = rs.getString("dept");
+	            System.out.print(empno + "\t");
+	            System.out.print(name + "\t\t");
+	            System.out.print(sal + "\t");
+	            System.out.println(dept);
+	         }
+			con.close();
+
+
+
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+    }
+}
